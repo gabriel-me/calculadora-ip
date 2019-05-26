6f9ee949ac00db5d35d9e56ad434706f1d6deec4
Gabriel Marques <gabriel.marquesdesouza28@gmail.com>
2019-05-26 18:27:15 -0300

    Adicionado função que converte IP em decimal para binário

--- calculator.js
+++ calculator.js
@@ -13,3 +13,38 @@ function getClass(ip) {
   else if (ip[0] > 191 && ip[0] < 224) return 'C';
   else return 'undefined';
 }
+
+function convertDecimalToBinary(ip) {
+  ip = ip.split('.');
+  let sum = 0;
+  const sequence = [128, 64, 32, 16, 8, 4, 2, 1];
+
+  let result = [
+    { decimal: ip[0], binary: '' },
+    { decimal: ip[1], binary: '' },
+    { decimal: ip[2], binary: '' },
+    { decimal: ip[3], binary: '' }
+  ];
+
+  result.forEach(octet => {
+    for (let i = 0; i < sequence.length; i++) {
+      if (sequence[i] < octet.decimal) {
+        octet.binary += 1;
+        sum = sequence[i];
+        for (let j = i + 1; j < sequence.length; j++) {
+          if (sum + sequence[j] <= octet.decimal) {
+            sum += sequence[j];
+            octet.binary += 1;
+          } else {
+            octet.binary += 0;
+          }
+        }
+        break;
+      } else {
+        octet.binary += 0;
+      }
+    }
+  });
+
+  return result;
+}
